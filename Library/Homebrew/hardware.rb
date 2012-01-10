b93e09cda27cb3b7e9dacb6110172f39a6be27b8
Konstantin Shabanov <etehtsea@gmail.com>
2012-01-10 05:51:02 +0400

    Refactor Hardware module with rspec tests

--- Library/Homebrew/hardware.rb
+++ Library/Homebrew/hardware.rb
@@ -1,43 +1,32 @@
 module Hardware
   # These methods use info spewed out by sysctl.
   # Look in <mach/machine.h> for decoding info.
+  CPU_TYPES = { 7 => :intel, 18 => :ppc }
+  INTEL_FAMILIES = {
+    0x73d67300 => :core,        # Yonah: Core Solo/Duo
+    0x426f69ef => :core2,       # Merom: Core 2 Duo
+    0x78ea4fbc => :penryn,      # Penryn
+    0x6b5a4cd2 => :nehalem,     # Nehalem
+    0x573B5EEC => :arrandale,   # Arrandale
+    0x5490B78C => :sandybridge, # Sandy bridge
+  }
 
   class << self
     def cpu_type
-      @cpu_type ||= `/usr/sbin/sysctl -n hw.cputype`.to_i
-
-      case @cpu_type
-      when 7 ; :intel
-      when 18; :ppc
-      else :dunno
-      end
+      @cpu_type ||= CPU_TYPES[sysctl_value 'hw.cputype'] || :dunno
     end
 
     def intel_family
-      @intel_family ||= `/usr/sbin/sysctl -n hw.cpufamily`.to_i
-
-      case @intel_family
-      when 0x73d67300; :core        # Yonah: Core Solo/Duo
-      when 0x426f69ef; :core2       # Merom: Core 2 Duo
-      when 0x78ea4fbc; :penryn      # Penryn
-      when 0x6b5a4cd2; :nehalem     # Nehalem
-      when 0x573B5EEC; :arrandale   # Arrandale
-      when 0x5490B78C; :sandybridge # Sandy bridge
-      else :dunno
-      end
+      @intel_family ||= INTEL_FAMILIES[sysctl_value 'hw.cpufamily'] || :dunno
     end
 
     def processor_count
-      @processor_count ||= `/usr/sbin/sysctl -n hw.ncpu`.to_i
+      @processor_count ||= sysctl_value 'hw.ncpu'
     end
 
     def cores_as_words
-      case processor_count
-      when 1; 'single'
-      when 2; 'dual'
-      when 4; 'quad'
-      else processor_count
-      end
+      quantity = { 1 => 'single', 2 => 'dual' , 4 => 'quad' }
+      @cores_as_words ||= quantity[processor_count] || processor_count
     end
 
     def is_32_bit?
@@ -45,7 +34,7 @@ module Hardware
     end
 
     def is_64_bit?
-      sysctl_bool("hw.cpu64bit_capable")
+      sysctl_value 'hw.cpu64bit_capable'
     end
 
     def bits
@@ -53,12 +42,8 @@ module Hardware
     end
 
     protected
-    def sysctl_bool(property)
-      result = nil
-      IO.popen("/usr/sbin/sysctl -n #{property} 2>/dev/null") do |f|
-        result = f.gets.to_i # should be 0 or 1
-      end
-      $?.success? && result == 1 # sysctl call succeded and printed 1
+    def sysctl_value(variable)
+      `/usr/sbin/sysctl -n #{variable}`.to_i
     end
   end
 end
---
+++ Library/Homebrew/spec/hardware_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+require 'extend/ARGV'
+ARGV.extend(HomebrewArgvExtension)
+
+require 'hardware'
+
+describe Hardware do
+  context :cpu_type do
+    subject { Hardware.cpu_type }
+
+    it { should be_a Symbol }
+
+    it "should be known" do
+      Hardware::CPU_TYPES.values.should include subject
+    end
+
+    it "should be dunno if unknown" do
+      Hardware.clone.tap do |o|
+        o.instance_eval do
+          @cpu_type = nil
+          def sysctl_value(variable); 0; end
+        end
+      end.cpu_type.should == :dunno
+    end
+  end
+
+  context :intel_family do
+    subject { Hardware.intel_family }
+
+    it { should be_a Symbol }
+
+    it "should be known" do
+      Hardware::INTEL_FAMILIES.values.should include subject
+    end
+
+    it "should be dunno if unknown" do
+      Hardware.clone.tap do |o|
+        o.instance_eval do
+          @intel_family = nil
+          def sysctl_value(variable); 0; end
+        end
+      end.intel_family.should == :dunno
+    end
+  end if Hardware.cpu_type == :intel
+
+  its(:processor_count) { should == Hardware.send(:sysctl_value, 'hw.ncpu') }
+
+  context :cores_as_words do
+    subject { Hardware.cores_as_words }
+
+    it { should be_a String }
+
+    { 1 => 'single', 2 => 'dual', 4 => 'quad' }.each do |k, v|
+      specify do
+        Hardware.clone.tap do |o|
+          o.instance_eval do
+            @cores_as_words = nil
+            eval("def processor_count; #{k}; end")
+          end
+        end.cores_as_words.should == v
+      end
+    end
+  end
+end

29deb51b3a9c37273d0f1d73f5d9ec6864b94246
Konstantin Shabanov <etehtsea@gmail.com>
2012-01-17 03:08:30 +0400

    Refactor brew-fetch

--- Library/Homebrew/cmd/fetch.rb
+++ Library/Homebrew/cmd/fetch.rb
@@ -1,49 +1,49 @@
 require 'formula'
 
 # Downloads the tarballs for the given formulae to the Cache
-
 module Homebrew extend self
   def fetch
-    if ARGV.include? '--deps'
-      bucket = []
-      ARGV.formulae.each do |f|
-        bucket << f
-        bucket << f.recursive_deps
-      end
+    Fetcher.new(ARGV).process
+  end
+end
 
-      bucket = bucket.flatten.uniq
-    else
-      bucket = ARGV.formulae
-    end
+class Fetcher
+  def initialize(argv)
+    @argv = argv
+  end
 
+  def process
     puts "Fetching: #{bucket * ', '}" if bucket.size > 1
 
     bucket.each do |f|
-      if ARGV.include? "--force" or ARGV.include? "-f"
-        where_to = f.cached_download
-        FileUtils.rm_rf where_to if File.exist? where_to
+      force(f) if @argv.include? "--force" or @argv.include? "-f"
+      tarball, _ = f.fetch
+      next unless tarball.kind_of? Pathname
+
+      Pathname::Checksum::TYPES.each do |type|
+        puts "#{type.to_s.upcase}: #{tarball.send(type)}"
       end
 
-      the_tarball, _ = f.fetch
-      next unless the_tarball.kind_of? Pathname
+      formula_checksum = f.instance_variable_get("@#{f.checksum_type}")
+      tarball_checksum = tarball.send(f.checksum_type)
 
-      previous_md5 = f.instance_variable_get(:@md5)
-      previous_sha1 = f.instance_variable_get(:@sha1)
-      previous_sha256 = f.instance_variable_get(:@sha256)
-
-      puts "MD5:  #{the_tarball.md5}"
-      puts "SHA1: #{the_tarball.sha1}"
-      puts "SHA256: #{the_tarball.sha256}"
-
-      unless previous_md5.nil? or previous_md5.empty? or  the_tarball.md5 == previous_md5
-        opoo "Formula reports different MD5: #{previous_md5}"
-      end
-      unless previous_sha1.nil? or previous_sha1.empty? or the_tarball.sha1 == previous_sha1
-        opoo "Formula reports different SHA1: #{previous_sha1}"
-      end
-      unless previous_sha256.nil? or previous_sha256.empty? or  the_tarball.sha256 == previous_sha256
-        opoo "Formula reports different SHA256: #{previous_sha256}"
+      unless formula_checksum == tarball_checksum
+        opoo "Formula reports different #{f.checksum_type}: #{tarball_checksum}"
       end
     end
+  end
+
+  private
+  def bucket
+    @bucket ||= if @argv.include? '--deps'
+      [@argv.formulae, @argv.formulae.map(&:recursive_deps)].flatten.uniq
+    else
+      @argv.formulae
+    end
+  end
+
+  def force(f)
+    where_to = f.cached_download
+    FileUtils.rm_rf where_to if File.exist? where_to
   end
 end

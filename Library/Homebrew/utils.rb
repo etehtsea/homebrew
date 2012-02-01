81439fe97b73f167ef56a0415f4732ece1b419ab
Konstantin Shabanov <etehtsea@gmail.com>
2012-02-01 22:04:57 +0400

    Always treat object as Array

--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -39,9 +39,7 @@ module Utils
 
     # Removes variable assignments completely.
     def remove_var!(flags)
-      # Next line is for Ruby 1.9.x compatibility
-      flags = [flags] unless flags.kind_of? Array
-      flags.each do |flag|
+      Array(flags).each do |flag|
         # Also remove trailing \n, if present.
         gsub! Regexp.new("^#{flag}[ \\t]*=(.*)$\n?"), ""
       end

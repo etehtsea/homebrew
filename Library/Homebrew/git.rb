90291b1ee87532cbfa7065272c8e8c0a22387883
Konstantin Shabanov <etehtsea@gmail.com>
2012-02-15 01:29:08 +0400

    Extract git stuff to own class

---
+++ Library/Homebrew/git.rb
@@ -0,0 +1,66 @@
+module Git
+  class Repo
+    def initialize(dir, url)
+      unless system "/usr/bin/which -s git"
+        abort "Please `brew install git' first."
+      end
+
+      @url = url
+      Dir.chdir(dir)
+    end
+
+    def exists?
+      !Dir['.git/*'].empty?
+    end
+
+    def init
+      begin
+        safe_system "git init"
+        safe_system "git remote add origin #{@url}"
+        safe_system "git fetch origin"
+        safe_system "git reset --hard origin/master"
+      rescue Exception
+        safe_system "/bin/rm -rf .git"
+        raise
+      end
+    end
+
+    def head
+      execute("git rev-parse HEAD").chomp
+    end
+
+    def add_origin
+      # originally we fetched by URL but then we decided that we should
+      # use origin so that it's easier for forks to operate seamlessly
+      unless `git remote`.split.include? 'origin'
+        safe_system "git remote add origin #{@url}"
+      end
+    end
+
+    def checkout(arg = 'master')
+      safe_system "git checkout -q #{arg}"
+    end
+
+    def pull
+      # specify a refspec so that 'origin/master' gets updated
+      refspec = "refs/heads/master:refs/remotes/origin/master"
+      rebase = "--rebase" if ARGV.include? "--rebase"
+      execute "git pull #{rebase} origin #{refspec}"
+    end
+
+    def changes(initial, current)
+      execute("git diff-tree -r --name-status -z #{initial} #{current}").split("\0")
+    end
+
+    private
+    def execute(cmd)
+      out = `#{cmd}`
+      if $? && !$?.success?
+        $stderr.puts out
+        raise "Failed while executing #{cmd}"
+      end
+      ohai(cmd, out) if ARGV.verbose?
+      out
+    end
+  end
+end
--- Library/Homebrew/updater.rb
+++ Library/Homebrew/updater.rb
@@ -1,56 +1,56 @@
 class Updater
   def initialize
-    abort "Please `brew install git' first." unless system "/usr/bin/which -s git"
-
-    report
+    if update!
+      report(title, title_type)
+    else
+      puts "Already up-to-date."
+    end
   end
 
-  def git_repo?
-    Dir['.git/*'].length > 0
+  private
+  def report
+    puts "Updated #{@title} from #{@initial[0,8]} to #{@current[0,8]}."
+
+    # get installed formulas list
+    installed = Homebrew.cellar.children.
+      select { |pn| pn.directory? }.
+      map    { |pn| pn.basename.to_s }.sort if Homebrew.cellar.directory?
+
+    @changes.each do |type, changes|
+      unless changes.empty?
+        ohai("#{type} #{@title_type}")
+
+        if installed.any?
+          changes.map! { |item| installed.include?(item) ? "#{item}*" : item }
+        end
+
+        puts_columns changes
+      end
+    end
   end
 
   # Performs an update of the homebrew source. Returns +true+ if a newer
   # version was available, +false+ if already up-to-date.
   def update!
-    initial, current = nil, nil
+    repo = Git::Repo.new(@repo_dir, @repo_url)
 
-    @settings[:repo_dir].cd do
-      if git_repo?
-        safe_system "git checkout -q master"
-        initial = read_revision
-        # originally we fetched by URL but then we decided that we should
-        # use origin so that it's easier for forks to operate seamlessly
-        unless `git remote`.split.include? 'origin'
-          safe_system "git remote add origin #{@settings[:repo_url]}"
-        end
-      else
-        begin
-          safe_system "git init"
-          safe_system "git remote add origin #{@settings[:repo_url]}"
-          safe_system "git fetch origin"
-          safe_system "git reset --hard origin/master"
-        rescue Exception
-          safe_system "/bin/rm -rf .git"
-          raise
-        end
-      end
-
-      # specify a refspec so that 'origin/master' gets updated
-      refspec = "refs/heads/master:refs/remotes/origin/master"
-      rebase = "--rebase" if ARGV.include? "--rebase"
-      execute "git pull #{rebase} origin #{refspec}"
-
-      current = read_revision
+    if repo.exists?
+      repo.checkout
+      @initial = repo.head
+      repo.add_origin
+    else
+      repo.init
     end
 
-    if initial && initial != current
+    repo.pull
+    @current = repo.head
+
+    if @initial && @initial != @current
       # hash with status characters for keys:
       # Added (A), Deleted (D), Modified (M)
       @changes_map = Hash.new { |h, k| h[k] = [] }
 
-      changes = @settings[:repo_dir].cd do
-        execute("git diff-tree -r --name-status -z #{initial} #{current}").split("\0")
-      end
+      changes = repo.changes(@initial, @current)
 
       while status = changes.shift
         file = changes.shift
@@ -58,49 +58,15 @@ class Updater
       end
 
       if @changes_map.any?
-        @changes = { 'New'     => changed_items('A', @settings[:track_dir]),
-                     'Updated' => changed_items('M', @settings[:track_dir]),
-                     'Deleted' => changed_items('D', @settings[:track_dir]) }
-
-        return [initial, current]
+        @changes = { 'New'     => changed_items('A', @track_dir),
+                     'Updated' => changed_items('M', @track_dir),
+                     'Deleted' => changed_items('D', @track_dir) }
+        return true
       end
-
     end
 
     # assume nothing was updated
     return false
-  end
-
-  def report
-    revisions = update!
-
-    if revisions
-      puts "Updated #{@settings[:title]} from #{revisions.first[0,8]} to #{revisions.last[0,8]}."
-
-      # get installed formulas list
-      installed = Homebrew.cellar.children.
-        select { |pn| pn.directory? }.
-        map    { |pn| pn.basename.to_s }.sort if Homebrew.cellar.directory?
-
-      @changes.each do |type, changes|
-        unless changes.empty?
-          ohai("#{type} #{@settings[:title_type]}")
-
-          if installed.any?
-            changes.map! { |item| installed.include?(item) ? "#{item}*" : item }
-          end
-
-          puts_columns changes
-        end
-      end
-    else
-      puts "Already up-to-date."
-    end
-  end
-
-  private
-  def read_revision
-    execute("git rev-parse HEAD").chomp
   end
 
   def filter_by_directory(files, dir)
@@ -114,27 +80,15 @@ class Updater
   def changed_items(status, dir)
     basenames(filter_by_directory(@changes_map[status], dir)).sort
   end
-
-  def execute(cmd)
-    out = `#{cmd}`
-    if $? && !$?.success?
-      $stderr.puts out
-      raise "Failed while executing #{cmd}"
-    end
-    ohai(cmd, out) if ARGV.verbose?
-    out
-  end
 end
 
 class UpdateFormulary < Updater
   def initialize
-    @settings = {
-      :title      => 'Formulary',
-      :repo_url   => 'https://github.com/etehtsea/formulary.git',
-      :repo_dir   => Homebrew.formulary,
-      :track_dir  => 'Formula/',
-      :title_type => 'formulae'
-    }
+    @title      = 'Formulary',
+    @repo_url   = 'https://github.com/etehtsea/formulary.git',
+    @repo_dir   = Homebrew.formulary,
+    @track_dir  = 'Formula/',
+    @title_type = 'formulae'
 
     super
   end
@@ -142,13 +96,11 @@ end
 
 class UpdateBrew < Updater
   def initialize
-    @settings = {
-      :title      => 'Homebrew',
-      :repo_url   => 'https://github.com/etehtsea/homebrew.git',
-      :repo_dir   => Homebrew.repository,
-      :track_dir  => 'Library/Homebrew/cmd',
-      :title_type => 'commands'
-    }
+    @title      = 'Homebrew',
+    @repo_url   = 'https://github.com/etehtsea/homebrew.git',
+    @repo_dir   = Homebrew.repository,
+    @track_dir  = 'Library/Homebrew/cmd',
+    @title_type = 'commands'
 
     super
   end

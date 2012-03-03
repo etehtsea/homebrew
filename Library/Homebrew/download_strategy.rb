735eb0e6b8efacdbcab74d1be09f751e0966997f
Konstantin Shabanov <etehtsea@gmail.com>
2012-03-03 16:33:57 +0400

    typo fix

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -387,8 +387,8 @@ module DownloadStrategy
           safe_system 'git', 'submodule', '--quiet', 'foreach', '--recursive', sub_cmd
         end
       end
+      ENV['GIT_DIR'] = cached_location+'.git'
     end
-    ENV['GIT_DIR'] = cached_location+'.git'
   end
 
   class CVS < Abstract

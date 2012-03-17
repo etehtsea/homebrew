8888d77a258e90109c4e9967e2ddac30b9168480
Konstantin Shabanov <etehtsea@gmail.com>
2012-03-17 13:34:06 +0400

    Remove problematic string specs

--- Library/Homebrew/spec/extend/string_spec.rb
+++ Library/Homebrew/spec/extend/string_spec.rb
@@ -1,8 +1,4 @@
 require 'spec_helper'
-# just to check own String#start_with?
-# because it was introduces in RUBY >= 1.8.7
-class StdString < String; end
-class String; undef_method(:start_with?); end
 require 'extend/string'
 
 describe String do
@@ -24,18 +20,6 @@ describe String do
   context "#start_with?" do
     it 'should be defined' do
       String.method_defined?(:start_with?).should be_true
-    end
-
-    context "should work as std one for" do
-      it 'strings' do
-        test_result = 'my friend over'.start_with?('my friend')
-        std_result  = StdString.new('my friend over').start_with?('my friend')
-        test_result.should eq std_result
-      end
-
-      it 'not strings' do
-        '4'.start_with?(4).should eq StdString.new('4').start_with?(4)
-      end
     end
   end
 end

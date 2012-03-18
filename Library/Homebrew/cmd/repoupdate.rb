ead3183fa4e4952e113164bf8a5ca9de14de1a31
Konstantin Shabanov <etehtsea@gmail.com>
2012-03-19 02:26:08 +0400

    typo fix

--- Library/Homebrew/cmd/repoupdate.rb
+++ Library/Homebrew/cmd/repoupdate.rb
@@ -4,9 +4,9 @@ module Homebrew extend self
   def repoupdate
     formularies.each do |name, settings|
       Updater.new do |u|
-        u.title      = name
+        u.title      = "#{name.capitalize} formulary"
         u.repo_url   = settings['url']
-        u.repo_dir   = Homebrew.formularies_path + settings['path']
+        u.repo_dir   = Homebrew.formularies_path + name
         u.track_dir  = settings['track_dir']
       end
     end
